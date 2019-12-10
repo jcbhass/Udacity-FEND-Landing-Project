5292296a42b491276b2320be160a46c479736c60
jcbhass <47954686+jcbhass@users.noreply.github.com>
2019-12-10 13:32:46 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,37 +2,36 @@ const navUl  = document.getElementById('navbar__list'); //style class id
 const navSections = document.getElementsByTagName('section');//get section
 const navMenu = document.getElementsByTagName('nav'); //get navbar
 
+//create navbar with styling elements and link
 function navMenuBar(section) {
     for(i = 0; i < navSections.length; i++) {
-        var eachSection = navSections[i]; //separate to iterate over
+        var eachSection = navSections[i]; 
         
         if (eachSection.hasAttribute('data-nav')) {
-            var line = document.createElement('li'); // creates list element
-            line.classList.add('navbar__menu'); //adds style class to list element
-            line.classList.add('menu__link'); //adds style hover class
-            //line.classList.add('your-active-class');
-            var sectionInfo = eachSection.getAttribute('data-nav'); //gets the data-nav text info identifying each section
-            var sectionName = document.createTextNode(sectionInfo); //defines the section name
+            var line = document.createElement('li'); 
+            line.classList.add('navbar__menu'); 
+            line.classList.add('menu__link'); 
+            var sectionInfo = eachSection.getAttribute('data-nav'); 
+            var sectionName = document.createTextNode(sectionInfo); 
             
-            var anchor = document.createElement('a'); //create anchor element 
-            var sectionId = eachSection.getAttribute('id'); //go to id instead of creating href-link
-            line.appendChild(anchor); //attach anchor to list
-            line.appendChild(sectionName); //adds the section name to the list
+            var anchor = document.createElement('a'); 
+            var sectionId = eachSection.getAttribute('id'); 
+            line.appendChild(anchor); 
+            line.appendChild(sectionName); 
 
             //go to section when navbar is clicked. Also activate the section styles when viewing.
+            let eachLink = document.getElementById(sectionId);
             line.addEventListener('click', function() {
-                document.getElementById(sectionId).scrollIntoView({
+                eachLink.scrollIntoView({
                     behavior: 'smooth'
-                });
-                sectionId.style = ('section.your-active-class');//add active class when clicked. 
-                sectionId.style = ('landing__container');
-            });
-            
-            navUl.appendChild(line); //adds the list to the unordered list element
-            
+                })
+                //eachLink.classList.add('activated');
+            })
         
         };
-        
+        navUl.appendChild(line); //adds the list to the unordered list element
+
+
     };    
 
 };
@@ -41,23 +40,25 @@ function navMenuBar(section) {
 
 navMenuBar();
 
+//Activate each section as they are scrolled into view 
 
-//highlight navbar when reading that section
+window.addEventListener('scroll', inView);
 
-// // As seen here: https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/ 
+function inView() {
+    for (var i = 0; i < navSections.length; i++) {
+    var viewed = navSections[i]; 
+    var heading = viewed.querySelector('h2');
+    var position = viewed.getBoundingClientRect().top;
 
-// var isInViewport = function (elem) {
-//   var bounding = elem.getBoundingClientRect();
-//   return (
-//       bounding.top >= 0 &&
-//       bounding.left >= 0 &&
-//       bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//       bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// };
-
-// const bounding = navMenuItems.getBoundingClientRect;
-// console.log(bounding);
+        if (position <= 400) {
+            viewed.classList.add('your-active-class');
+            heading.style.color = '#0f0';
+        } else {
+            viewed.classList.remove('your-active-class');
+            heading.style.color = '#fff';
+        }
+    }
+}
 
 
-
+inView();
